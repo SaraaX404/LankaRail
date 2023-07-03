61f93000f4a81c5b170a3d638eba85cd37f7a0c2
SaraaX404 <thathsarasenanayaka74@gmail.com>
2023-07-03 23:43:47 +0530

    refactor:Replace React-Native components from Native-Base Components

--- Screens/BookingSeats/BookingSeats.tsx
+++ Screens/BookingSeats/BookingSeats.tsx
@@ -1,117 +1,214 @@
- 
-import React from 'react'
-import { View, Button, Text, Image, ScrollView,  } from 'native-base';
-import {TouchableOpacity, StyleSheet} from 'react-native'
+import React from "react";
+import { View, Button, Text, Image, ScrollView } from "native-base";
+import { TouchableOpacity, StyleSheet } from "react-native";
 
 // Styles
-import { GlobalBackgroundColors, globalShadowBox, primaryButton, GlobalBackgroundTextColors } from '../../Styles/global';
+import {
+  GlobalBackgroundColors,
+  globalShadowBox,
+  primaryButton,
+  GlobalBackgroundTextColors,
+} from "../../Styles/global";
 //Components
-import Seat from '../../assets/Images/seats';
-export default() => {
+import Seat from "../../assets/Images/seats";
+export default () => {
   return (
-    <View   >
-    {/* train Detail */}
-    <View style={globalShadowBox} mt={10}  mb={10} mx={'auto'}  flexDirection={'column'} justifyContent={'space-between'}  width={'90%'}  p={5} backgroundColor={GlobalBackgroundColors.ternaryColor}>
-        <Text mx={'auto'} fontSize={25} fontWeight={'bold'}>Rs.450</Text>
-        <View flexDirection={'row'} justifyContent={'space-between'} mt={3}>
-            <Text mt={'auto'} mb={'auto'} fontSize={20}>Matara</Text>
-            <Image width={'50px'} height={'40px'} source={require('../../assets/Images/train1.jpg')} alt={'image'} />
-            <Text marginTop={'auto'} marginBottom={'auto'} fontSize={20}>Colombo Fort</Text>
+    <View>
+      {/* train Detail */}
+      <View
+        style={globalShadowBox}
+        mt={10}
+        mb={10}
+        mx={"auto"}
+        flexDirection={"column"}
+        justifyContent={"space-between"}
+        width={"90%"}
+        p={5}
+        backgroundColor={GlobalBackgroundColors.ternaryColor}
+      >
+        <Text mx={"auto"} fontSize={25} fontWeight={"bold"}>
+          Rs.450
+        </Text>
+        <View flexDirection={"row"} justifyContent={"space-between"} mt={3}>
+          <Text mt={"auto"} mb={"auto"} fontSize={20}>
+            Matara
+          </Text>
+          <Image
+            width={"50px"}
+            height={"40px"}
+            source={require("../../assets/Images/train1.jpg")}
+            alt={"image"}
+          />
+          <Text marginTop={"auto"} marginBottom={"auto"} fontSize={20}>
+            Colombo Fort
+          </Text>
         </View>
-        <View flexDirection={'row'} justifyContent={'space-between'} mt={3}  pl={30}  pr={30}>
-            <Text fontSize={'10px'} >5.00 AM</Text>
-            <Text fontSize={'10px'} >9.00AM</Text>
+        <View
+          flexDirection={"row"}
+          justifyContent={"space-between"}
+          mt={3}
+          pl={30}
+          pr={30}
+        >
+          <Text fontSize={"10px"}>5.00 AM</Text>
+          <Text fontSize={"10px"}>9.00AM</Text>
         </View>
-    </View>
-    {/* Seats */}
+      </View>
+      {/* Seats */}
 
-    <View flexDirection={'row'}  justifyContent={'space-around'} marginTop={'5px'}>
-            <View flexDirection={'row'} alignItems={'center'} >
-                <View style={[styles.SeatContainer, globalShadowBox, { width: 30, height: 30 }]}>
+      <View
+        flexDirection={"row"}
+        justifyContent={"space-around"}
+        marginTop={"5px"}
+      >
+        <View flexDirection={"row"} alignItems={"center"}>
+          <View
+            style={[
+              styles.SeatContainer,
+              globalShadowBox,
+              { width: 30, height: 30 },
+            ]}
+          >
+            <Seat color={0}></Seat>
+          </View>
+          <Text ml={"10px"}>Available</Text>
+        </View>
+
+        <View flexDirection={"row"} alignItems={"center"}>
+          <View
+            style={[
+              styles.SeatContainer,
+              globalShadowBox,
+              { width: 30, height: 30 },
+            ]}
+          >
+            <Seat color={-1}></Seat>
+          </View>
+          <Text ml={"10px"}>Booked</Text>
+        </View>
+      </View>
+
+      <View mt={"10px"}>
+        <ScrollView>
+          <View>
+            {/* Seats Goes Here */}
+            <View
+              width={"90%"}
+              mx={"auto"}
+              mb={20}
+              flex={1}
+              backgroundColor={GlobalBackgroundColors.ternaryColor}
+              style={[globalShadowBox]}
+            >
+              {/* <SeatInfo /> */}
+              <Text
+                color={"red"}
+                textAlign={"center"}
+                marginTop={"5px"}
+                marginBottom={"5px"}
+                fontWeight={"bold"}
+              >
+                {" "}
+              </Text>
+
+              {/* Seats*/}
+              <ScrollView>
+                <View flexDirection={"row"} flexWrap={"wrap"}>
+                  {/* {trainSeatDataComponent} */}
+                  <View
+                    style={[
+                      styles.SeatContainer,
+                      globalShadowBox,
+                      { width: 30, height: 30 },
+                    ]}
+                  >
                     <Seat color={0}></Seat>
-                    
-                </View>
-                <Text ml={'10px'}>Available</Text>
-            </View>
-            
-            <View flexDirection={'row'} alignItems={'center'}>
-                <View style={[styles.SeatContainer, globalShadowBox, { width: 30, height: 30 }]}>
+                  </View>
+                  <View
+                    style={[
+                      styles.SeatContainer,
+                      globalShadowBox,
+                      { width: 30, height: 30 },
+                    ]}
+                  >
                     <Seat color={-1}></Seat>
+                  </View>
+
+                  <View
+                    style={[
+                      styles.SeatContainer,
+                      globalShadowBox,
+                      { width: 30, height: 30 },
+                    ]}
+                  >
+                    <Seat color={0}></Seat>
+                  </View>
+                  <View
+                    style={[
+                      styles.SeatContainer,
+                      globalShadowBox,
+                      { width: 30, height: 30 },
+                    ]}
+                  >
+                    <Seat color={0}></Seat>
+                  </View>
+                  <View
+                    style={[
+                      styles.SeatContainer,
+                      globalShadowBox,
+                      { width: 30, height: 30 },
+                    ]}
+                  >
+                    <Seat color={0}></Seat>
+                  </View>
+                  <View
+                    style={[
+                      styles.SeatContainer,
+                      globalShadowBox,
+                      { width: 30, height: 30 },
+                    ]}
+                  >
+                    <Seat color={0}></Seat>
+                  </View>
+                  <View
+                    style={[
+                      styles.SeatContainer,
+                      globalShadowBox,
+                      { width: 30, height: 30 },
+                    ]}
+                  >
+                    <Seat color={-1}></Seat>
+                  </View>
                 </View>
-                <Text ml={'10px'}>Booked</Text>
+              </ScrollView>
+
+              {/* Butons */}
+              <View mt={"20px"} mb={"10px"}>
+                <TouchableOpacity
+                  style={[{ marginLeft: "auto", marginRight: "auto" }]}
+                >
+                  <Text
+                    textAlign={"center"}
+                    color={GlobalBackgroundTextColors.secondaryColor}
+                  >
+                    Book Now
+                  </Text>
+                </TouchableOpacity>
+              </View>
             </View>
-        </View>
-
-    
-
-    <View mt={'10px'}>
-    <ScrollView>
-                    <View>
-                         
-                        {/* Seats Goes Here */}
-                        <View width={'90%'}  mx={'auto'} mb={20} flex={1} backgroundColor={GlobalBackgroundColors.ternaryColor} style={[globalShadowBox]}>
-                            {/* <SeatInfo /> */}
-                            <Text color={'red'} textAlign={'center'} marginTop={'5px'} marginBottom={'5px'} fontWeight={'bold'} >   </Text>
-
-
-                            {/* Seats*/}
-                            <ScrollView>
-                                <View flexDirection={'row'} flexWrap={'wrap'}>
-                                    {/* {trainSeatDataComponent} */}
-                                      <View style={[styles.SeatContainer, globalShadowBox, { width: 30, height: 30 }]}>
-                                       <Seat color={0}></Seat>
-                    
-                                      </View>
-                                      <View style={[styles.SeatContainer, globalShadowBox, { width: 30, height: 30 }]}>
-                                       <Seat color={-1}></Seat>
-                    
-                                      </View>
-
-                                      <View style={[styles.SeatContainer, globalShadowBox, { width: 30, height: 30 }]}>
-                                       <Seat color={0}></Seat>
-                    
-                                      </View>
-                                      <View style={[styles.SeatContainer, globalShadowBox, { width: 30, height: 30 }]}>
-                                       <Seat color={0}></Seat>
-                    
-                                      </View>
-                                      <View style={[styles.SeatContainer, globalShadowBox, { width: 30, height: 30 }]}>
-                                       <Seat color={0}></Seat>
-                    
-                                      </View>
-                                      <View style={[styles.SeatContainer, globalShadowBox, { width: 30, height: 30 }]}>
-                                       <Seat color={0}></Seat>
-                                      
-                                      </View>
-                                      <View style={[styles.SeatContainer, globalShadowBox, { width: 30, height: 30 }]}>
-                                       <Seat color={-1}></Seat>
-                    
-                                      </View>
-                                </View>
-                            </ScrollView>
-
-
-                            {/* Butons */}
-                            <View mt={'20px'} mb={'10px'} >
-                                <TouchableOpacity   style={[  { marginLeft: 'auto', marginRight: 'auto' }]}><Text textAlign={'center'} color={GlobalBackgroundTextColors.secondaryColor}>Book Now</Text></TouchableOpacity>
-                            </View>
-                        </View>
-                    </View>
-                </ScrollView>
-                </View>
-    
-</View>
+          </View>
+        </ScrollView>
+      </View>
+    </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-    SeatContainer: {
-        backgroundColor: GlobalBackgroundColors.ternaryColor,
-        padding: 5,
-        width: '100%',
-        height: '100%',
-        borderRadius: 10
-    }
+  SeatContainer: {
+    backgroundColor: GlobalBackgroundColors.ternaryColor,
+    padding: 5,
+    width: "100%",
+    height: "100%",
+    borderRadius: 10,
+  },
 });
-
- 
--- Screens/TrainSearchResult/TrainSearchResult.tsx
+++ Screens/TrainSearchResult/TrainSearchResult.tsx
@@ -1,60 +1,136 @@
-import { StyleSheet, Text, View,TouchableOpacity,Image } from 'react-native'
-import React from 'react'
-import { GlobalBackgroundColors, GlobalBackgroundTextColors ,globalShadowBox} from '../../Styles/global';
-import { useNavigation } from '@react-navigation/native'
-import {nav} from '../../Models'
+import { Text, View, Image } from "native-base";
+import { TouchableOpacity } from "react-native";
+import React from "react";
+import {
+  GlobalBackgroundColors,
+  GlobalBackgroundTextColors,
+  globalShadowBox,
+} from "../../Styles/global";
+import { useNavigation } from "@react-navigation/native";
+import { nav } from "../../Models";
 const TrainSearchResult = () => {
-    const {navigate} = useNavigation<nav>()
+  const { navigate } = useNavigation<nav>();
 
-    const navigateBookingSeats =()=>{
-        navigate('BookingSeats')
-    } 
+  const navigateBookingSeats = () => {
+    navigate("BookingSeats");
+  };
   return (
     <View>
-       
-      <TouchableOpacity  onPress={navigateBookingSeats }   style={[globalShadowBox, { width: '90%', marginLeft: 'auto', marginRight: 'auto', backgroundColor: 'white', borderRadius: 30, padding: 10, marginTop: 30 }]}>
-            {/* Bus Detail */}
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10 }}>
-                <Text style={{ fontWeight: 'bold', fontSize: 25, color: GlobalBackgroundColors.primaryColor }}>Ruhunu Kumari</Text>
-               
-                <Text style={{ fontWeight: 'bold', fontSize: 25, color: GlobalBackgroundColors.primaryColor }}>Rs.450</Text>
-            </View>
-            {/* Traveling Detail */}
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10 }}>
-            
-                <Text>Matara</Text>
-                <Image style={{ width: 50, height: 40 }} source={require('../../assets/Images/train.png')} />
-                <Text>Colombo Fort</Text>
-            </View>
-            {/* Time Detail */}
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10 }}>
-                <Text>5.00 Am</Text>
-                <Text>9.00Am</Text>
-            </View>
-          </TouchableOpacity>
-          
-          <TouchableOpacity   style={[globalShadowBox, { width: '90%', marginLeft: 'auto', marginRight: 'auto', backgroundColor: 'white', borderRadius: 30, padding: 10, marginTop: 30 }]}>
-            {/* Bus Detail */}
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10 }}>
-                <Text style={{ fontWeight: 'bold', fontSize: 25, color: GlobalBackgroundColors.primaryColor }}>Ruhunu Kumari</Text>
-                <Text style={{ fontWeight: 'bold', fontSize: 25, color: GlobalBackgroundColors.primaryColor }}>Rs.450</Text>
-            </View>
-            {/* Traveling Detail */}
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10 }}>
-                <Text>Matara</Text>
-                <Image style={{ width: 50, height: 40 }} source={require('../../assets/Images/train.png')} />
-                <Text>Colombo Fort</Text>
-            </View>
-            {/* Time Detail */}
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10 }}>
-                <Text>5.00 Am</Text>
-                <Text>9.00Am</Text>
-            </View>
-          </TouchableOpacity>
+      <TouchableOpacity
+        onPress={navigateBookingSeats}
+        style={[
+          globalShadowBox,
+          {
+            width: "90%",
+            marginLeft: "auto",
+            marginRight: "auto",
+            backgroundColor: "white",
+            borderRadius: 30,
+            padding: 10,
+            marginTop: 30,
+          },
+        ]}
+      >
+        {/* Bus Detail */}
+        <View
+          flexDirection={"row"}
+          justifyContent={"space-between"}
+          mt={"10px"}
+        >
+          <Text
+            fontWeight={"bold"}
+            fontSize={25}
+            color={GlobalBackgroundColors.primaryColor}
+          >
+            Ruhunu Kumari
+          </Text>
+
+          <Text
+            fontWeight={"bold"}
+            fontSize={25}
+            color={GlobalBackgroundColors.primaryColor}
+          >
+            Rs.450
+          </Text>
+        </View>
+        {/* Traveling Detail */}
+        <View
+          flexDirection={"row"}
+          justifyContent={"space-between"}
+          mt={"10px"}
+        >
+          <Text>Matara</Text>
+          <Image
+            style={{ width: 50, height: 40 }}
+            source={require("../../assets/Images/train.png")}
+            alt={"image"}
+          />
+          <Text>Colombo Fort</Text>
+        </View>
+        {/* Time Detail */}
+        <View
+          flexDirection="row"
+          justifyContent="space-between"
+          marginTop={"10px"}
+        >
+          <Text>5.00 Am</Text>
+          <Text>9.00Am</Text>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[
+          globalShadowBox,
+          {
+            width: "90%",
+            marginLeft: "auto",
+            marginRight: "auto",
+            backgroundColor: "white",
+            borderRadius: 30,
+            padding: 10,
+            marginTop: 30,
+          },
+        ]}
+      >
+        {/* Bus Detail */}
+        <View flexDirection="row" justifyContent="space-between" mt={"10px"}>
+          <Text
+            fontWeight={"bold"}
+            fontSize={25}
+            color={GlobalBackgroundColors.primaryColor}
+          >
+            Ruhunu Kumari
+          </Text>
+          <Text
+            fontWeight={"bold"}
+            fontSize={25}
+            color={GlobalBackgroundColors.primaryColor}
+          >
+            Rs.450
+          </Text>
+        </View>
+        {/* Traveling Detail */}
+        <View flexDirection="row" justifyContent="space-between" mt={"10px"}>
+          <Text>Matara</Text>
+          <Image
+            style={{ width: 50, height: 40 }}
+            alt="image"
+            source={require("../../assets/Images/train.png")}
+          />
+          <Text>Colombo Fort</Text>
+        </View>
+        {/* Time Detail */}
+        <View
+          flexDirection={"row"}
+          justifyContent={"space-between"}
+          marginTop={"10px"}
+        >
+          <Text>5.00 Am</Text>
+          <Text>9.00Am</Text>
+        </View>
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
-export default TrainSearchResult
-
-const styles = StyleSheet.create({})
+export default TrainSearchResult;
